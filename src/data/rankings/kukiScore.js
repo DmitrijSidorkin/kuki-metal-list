bbd19bdbd2a892cabd26e112ef4637b0bd86fdbc
Dmitrij Sidorkin <dmitrij.sidorkin95@gmail.com>
2025-12-08 17:26:41 +0200

    update kuki score

--- src/data/rankings/kukiScore.js
+++ src/data/rankings/kukiScore.js
@@ -1,69 +1,111 @@
-const kukiScore = {
-  "8.6 Blackout": 1,
-  "A Call Beyond": 2,
-  "A Hope In Hell": 3,
-  "A Line In The Dust": 4,
-  "Another Miracle": 5,
-  "Bad Luck": 6,
-  "Begin The Sacrifice": 7,
-  "Blastbeat Falafel": 8,
-  "Bliss In 7 Languages": 9,
-  "Braindead": 10,
-  "Burn A Miracle": 11,
-  "Caramel": 12,
-  "Driftwood": 13,
-  "Dying To Love": 14,
-  "End Of You": 15,
-  "Elevator Operator": 16,
-  "Flesh Stays Together": 17,
-  "For What It's Worth": 18,
-  "Frost Troll": 19,
-  "Gifted Every Strength": 20,
-  "God Fearing Man": 21,
-  "Green Serpent": 22,
-  "Half-Life": 23,
-  "Hate Cult Ritual": 24,
-  "Heathen": 25,
-  "House of Light": 26,
-  "I Believe": 27,
-  "If It's All The Same To You": 28,
-  "Instrument Of War": 29,
-  "Into Hell": 30,
-  "INVERTED SPEAR OF HEAVEN": 31,
-  "It Only Gets Darker": 32,
-  "Kubrick Stare": 33,
-  "Let Go": 34,
-  "Lights Out": 35,
-  "Midnight": 36,
-  "Midnight Messiah": 37,
-  "Miracle": 38,
-  "More Than Hate": 39,
-  "New Disease": 40,
-  "Nu Delhi": 41,
-  "Pale Moonlight": 42,
-  "Présage": 43,
-  "RATATATA": 44,
-  "Sacred": 45,
-  "Sacred Place": 46,
-  "Sargaso": 47,
-  "Scars Upon Scars": 48,
-  "Shadow People": 49,
-  "SHALLOW": 50,
-  "Silver Swarm": 51,
-  "Snowlover": 52,
-  "Soft Spine": 53,
-  "Souvenir": 54,
-  "Static": 55,
-  "Struck Dead": 56,
-  "The Tales Of War": 57,
-  "Things We Tell Ourselves in the Dark": 58,
-  "Twenty One Cars": 59,
-  "Two-Headed Trout": 60,
-  "Until There's Nothing Left": 61,
-  "Up From The Bottom": 62,
-  "Wake The Dead": 63,
-  "What we know": 64,
-  "Whatever Alright": 65,
 
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const kukiScore = {
+"8.6 Blackout": 62,
+"A Call Beyond": 54,
+"A Hope In Hell": 34,
+"A Line In The Dust": 18,
+"Another Miracle": 26,
+"Bad Luck": 9,
+"Begin The Sacrifice": 21,
+"Blastbeat Falafel": 20,
+"Bliss In 7 Languages": 37,
+"Braindead": 1,
+"Burn A Miracle": 49,
+"Caramel": 10,
+"Driftwood": 55,
+"Dying To Love": 41,
+"End Of You": 5,
+"Elevator Operator": 7,
+"Flesh Stays Together": 47,
+"For What It's Worth": 42,
+"Frost Troll": 60,
+"Gifted Every Strength": 50,
+"God Fearing Man": 2,
+"Green Serpent": 29,
+"Half-Life": 40,
+"Hate Cult Ritual": 51,
+"Heathen": 56,
+"House of Light": 13,
+"I Believe": 30,
+"If It's All The Same To You": 36,
+"Instrument Of War": 39,
+"Into Hell": 19,
+"INVERTED SPEAR OF HEAVEN": 59,
+"It Only Gets Darker": 24,
+"Kubrick Stare": 52,
+"Let Go": 53,
+"Lights Out": 57,
+"Midnight": 63,
+"Midnight Messiah": 23,
+"Miracle": 16,
+"More Than Hate": 27,
+"New Disease": 14,
+"Nu Delhi": 6,
+"Pale Moonlight": 17,
+"Présage": 38,
+"RATATATA": 3,
+"Sacred": 8,
+"Sacred Place": 61,
+"Sargaso": 46,
+"Scars Upon Scars": 58,
+"Shadow People": 32,
+"SHALLOW": 31,
+"Silver Swarm": 25,
+"Snowlover": 45,
+"Soft Spine": 15,
+"Souvenir": 12,
+"Static": 4,
+"Struck Dead": 43,
+"The Tales Of War": 28,
+"Things We Tell Ourselves in the Dark": 48,
+"Twenty One Cars": 64,
+"Two-Headed Trout": 33,
+"Until There's Nothing Left": 65,
+"Up From The Bottom": 35,
+"Wake The Dead": 22,
+"What we know": 11,
+"Whatever Alright": 44,};
 export default kukiScore;
